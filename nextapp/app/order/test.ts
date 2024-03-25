c6c9b453956927fb91953de82a3ed1d92324cce6
HurkiCorgi <al0826503@gmail.com>
2024-03-24 18:44:38 -0600

    Add categories and products to order app

---
+++ nextapp/app/order/test.ts
@@ -0,0 +1,105 @@
+export const categories = [
+    {
+        id: 1,
+        name: 'Coffee',
+        slug: 'cafe',
+    },
+    {
+        id: 2,
+        name: 'Donuts',
+        slug: 'dona',
+    },
+    {
+        id: 3,
+        name: 'Pizza',
+        slug: 'pizza',
+    },
+    {
+        id: 4,
+        name: 'Burger',
+        slug: 'hamburguesa',
+    },
+    {
+        id: 5,
+        name: 'Cake',
+        slug: 'pastel',
+    },
+    {
+        id: 6,
+        name: 'Cookie',
+        slug: 'galletas',
+    }
+];
+
+export const products = [
+    {
+        id: 1,
+        name: 'Americano',
+        category: 'Coffee',
+        price: 3.50,
+        image: 'cafe_01.jpg',
+    },
+    {
+        id: 2,
+        name: 'Latte',
+        category: 'Coffee',
+        price: 4.50,
+        image: 'cafe_02.jpg',
+    },
+    {
+        id: 3,
+        name: 'Cappuccino',
+        category: 'Coffee',
+        price: 4.00,
+        image: 'cafe_03.jpg',
+    },
+    {
+        id: 4,
+        name: 'Espresso',
+        category: 'Coffee',
+        price: 2.50,
+        image: 'cafe_04.jpg',
+    },
+    {
+        id: 5,
+        name: 'Mocha',
+        category: 'Coffee',
+        price: 5.00,
+        image: 'cafe_05.jpg',
+    },
+    {
+        id: 6,
+        name: 'Donut',
+        category: 'Donuts',
+        price: 1.50,
+        image: 'donas_01.jpg',
+    },
+    {
+        id: 7,
+        name: 'Pizza Margherita',
+        category: 'Pizza',
+        price: 8.50,
+        image: 'pizzas_01.jpg',
+    },
+    {
+        id: 8,
+        name: 'Hamburger',
+        category: 'Burger',
+        price: 6.50,
+        image: 'cafe_02.jpg',
+    },
+    {
+        id: 9,
+        name: 'Chocolate Cake',
+        category: 'Cake',
+        price: 4.50,
+        image: 'cafe_02.jpg',
+    },
+    {
+        id: 10,
+        name: 'Cookie',
+        category: 'Cookie',
+        price: 2.50,
+        image: 'cafe_02.jpg',
+    }
+];
